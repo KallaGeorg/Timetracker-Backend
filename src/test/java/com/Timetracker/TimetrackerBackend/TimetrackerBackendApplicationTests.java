a52ba0e5aa597f5db5b17f1647409b03dfb502b6
Georg <kalla.georg@yahoo.com>
2024-05-01 16:11:28 +0200

    initial commit

---
+++ src/test/java/com/Timetracker/TimetrackerBackend/TimetrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Timetracker.TimetrackerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
