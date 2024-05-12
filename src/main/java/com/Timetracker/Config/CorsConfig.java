4d7f73a41deccc680c95dd8910d552ef7271e148
Georg <kalla.georg@yahoo.com>
2024-05-12 22:21:08 +0200

    Update CorsConfig.java

--- src/main/java/com/Timetracker/Config/CorsConfig.java
+++ src/main/java/com/Timetracker/Config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://seashell-app-k8vgq.ondigitalocean.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
                 .allowedHeaders("*");
                 
